20cda809552f9350561af29ed14af2d671898366
Gunho Park <gunho1123@postech.ac.kr>
2025-12-19 08:16:47 +0900

    Internal change

--- codegemm/inference/do_tps.sh
+++ codegemm/inference/do_tps.sh
@@ -1,10 +1,10 @@
 # Enter the inference folder
 
 # (Optional) choose GPU
-export GPU=0
+GPU=0
 
-# model_name_or_path="meta-llama/Llama-3.1-8B"
-model_name_or_path="meta-llama/Llama-3.1-70B-Instruct"
+model_name_or_path="meta-llama/Llama-3.1-8B"
+# model_name_or_path="meta-llama/Llama-3.1-70B-Instruct"
 
 # ---------------------------
 # CodeGEMM backend
@@ -12,7 +12,7 @@ model_name_or_path="meta-llama/Llama-3.1-70B-Instruct"
 
 backend="codegemm"
 vec_len=4
-group_size=32
+group_size=128
 num_codebooks=1
 nbits_per_codebook=8
 
